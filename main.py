b2268ec0ff171097f2849a6761e2155856b721c1
Noyonika <noyonikagg170608@gmail.com>
2022-12-11 09:33:49 +0530

    C224

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for, request, render_template, jsonify
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template("/login.html")
+
+@app.route('/login', methods = ["POST"])
+
+def login():
+    username = request.json.get("username")
+    password = request.json.get("password")
+    with open("cred.csv", "a+") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow([username, password])
+    return jsonify({
+        "status":"success"
+    }), 201
+
+if __name__ == "__name__":
+    app.run(debug = True)
